cde9c37e0aa9689db62c99c58488ba3ca2e35c81
jnsprnw <6gqrbj@posteo.org>
2022-06-07 10:42:18 +0200

    Exclude Twitter and Xing

--- blc/blc.js
+++ blc/blc.js
@@ -8,7 +8,7 @@ let countLinks = 0;
 const URL = 'https://datavis.berlin';
 
 const checker = new HtmlUrlChecker({
-  excludedKeywords: ['//t.me/', '//linkedin.com/']
+  excludedKeywords: ['//t.me/', '//linkedin.com/', '//xing.com/', '//twitter.com/']
 }, {
 	error: (error) => {
   	console.log('error', { error })
